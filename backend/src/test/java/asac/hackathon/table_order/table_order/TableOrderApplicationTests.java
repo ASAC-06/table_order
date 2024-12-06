95b91fd2c5aa43af95ba94cfb7a8751bfca23c81
daejin94 <eowls1358@gmail.com>
2024-12-06 10:15:05 +0900

    Init: first commit

---
+++ backend/src/test/java/asac/hackathon/table_order/table_order/TableOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package asac.hackathon.table_order.table_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
